9a3b52df8d9364ea668974a1a241c703d11c6996
KBorkow <30339630+KBorkow@users.noreply.github.com>
2020-05-15 10:20:40 +0200

    klasa words: upadate metody readFile

--- app/src/main/java/edu/psmw/projekt/Words.kt
+++ app/src/main/java/edu/psmw/projekt/Words.kt
@@ -1,18 +1,29 @@
 package edu.psmw.projekt
 
+import android.content.Context
+import android.content.res.AssetManager
 import java.io.BufferedReader
 import java.io.File
+import java.io.InputStream
+
 
 class Words {
-    fun readFile(fileName: String): BufferedReader {
-        val bufferedReader: BufferedReader = File(fileName).bufferedReader()
-        return bufferedReader
-    }
-
-    fun getWord() {
-        val reader: BufferedReader = readFile("slowka/dom.txt")
-        val inputString = reader.use { it.readText() }
-        print(inputString)
+    val x: String = "X"
+    fun readFile(context: Context, fileName: String): String {
+        val am: AssetManager = context.assets
+        val `is`: InputStream = am.open(fileName)
+        val buffreader = BufferedReader(`is`.reader())
+        val content = StringBuilder()
+        try {
+            var line = buffreader.readLine()
+            while (line != null) {
+                content.append(line + "\n")
+                line = buffreader.readLine()
+            }
+        } finally {
+            buffreader.close()
+        }
+       return content.toString()
     }
 
 }
