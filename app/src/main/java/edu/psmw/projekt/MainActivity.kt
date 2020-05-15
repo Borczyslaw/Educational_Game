9a4d2b95a65718ba11eb9bc40bcc971fc0fae5ee
KBorkow <30339630+KBorkow@users.noreply.github.com>
2020-05-15 10:23:34 +0200

    MainActivity - dodanie pozwolenia odczytu z dysku oraz użycie metody readFile z klasy Words

--- app/src/main/java/edu/psmw/projekt/MainActivity.kt
+++ app/src/main/java/edu/psmw/projekt/MainActivity.kt
@@ -1,13 +1,22 @@
 package edu.psmw.projekt
 
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StrictMode.allowThreadDiskReads()
         setContentView(R.layout.activity_main)
+        val word = Words()
+        val s: String = word.readFile(this, "slowka/dom.txt")
+        label1.text = s.subSequence(0, s.indexOf("\n"))
+
     }
-    val read = Words()
+
 }
+
+
