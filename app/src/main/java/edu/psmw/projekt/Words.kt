9e75f7e2e09e2e69ecf76ef95ce8adb100a08432
KBorkow <30339630+KBorkow@users.noreply.github.com>
2020-05-15 11:24:49 +0200

    Rozwiniecie klasy Words

--- app/src/main/java/edu/psmw/projekt/Words.kt
+++ app/src/main/java/edu/psmw/projekt/Words.kt
@@ -5,25 +5,38 @@ import android.content.res.AssetManager
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
+import java.util.*
 
 
 class Words {
-    val x: String = "X"
-    fun readFile(context: Context, fileName: String): String {
+    fun readFile(context: Context, fileName: String): Array<String> {
         val am: AssetManager = context.assets
+        val array = Array<String>(30){"it =$it"}
         val `is`: InputStream = am.open(fileName)
         val buffreader = BufferedReader(`is`.reader())
         val content = StringBuilder()
+        var i=0
         try {
             var line = buffreader.readLine()
-            while (line != null) {
+            while (line != null && i<30) {
+                array.set(i, line)
                 content.append(line + "\n")
                 line = buffreader.readLine()
+                i++
             }
         } finally {
             buffreader.close()
         }
-       return content.toString()
+       return array
+    }
+
+    fun get_rand_word( context: Context, fileName: String): String
+    {
+        var array = readFile(context,fileName)
+        val rand = Random()
+        val x: Int = rand.nextInt(30)
+        return array[x]
+
     }
 
 }
