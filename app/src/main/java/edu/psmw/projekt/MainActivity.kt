2ff4f820179183bc63a818bc3465d99ceda8805a
KBorkow <30339630+KBorkow@users.noreply.github.com>
2020-05-17 14:53:01 +0200

    Ustawienie wygladu docelowego i dodanie teł(tła)

--- app/src/main/java/edu/psmw/projekt/MainActivity.kt
+++ app/src/main/java/edu/psmw/projekt/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.StrictMode
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.pierwwszy_ekran.*
 import java.io.File
 import java.util.*
 
@@ -16,145 +17,149 @@ class MainActivity : AppCompatActivity() {
     fun onClickTemp(word: Words, kat: String ): String
     {
         val rand_word = word.get_rand_word(this, "slowka/"+kat+".txt")
-        label1.text = rand_word
+        //label1.text = rand_word
         return rand_word
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         StrictMode.allowThreadDiskReads()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.pierwwszy_ekran)
         var path = "/obrazki/"
         val word = Words() //instancja
-        val button_Ubr = btnUbrania
-        button_Ubr.setOnClickListener {
-            val kat = "ubrania"
-            val good_word = onClickTemp(word, kat)
-            val image = ImageReader()
-            path += kat + "/"
-            val wrong_images = Array<String>(3){"it =$it"}
-            for (i in 0..2) {
-                wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+        btn_start_game.setOnClickListener{
+            setContentView(R.layout.activity_main)
+            val button_Ubr = btn_ubrania
+            button_Ubr.setOnClickListener {
+                val kat = "ubrania"
+                val good_word = onClickTemp(word, kat)
+                val image = ImageReader()
+                path += kat + "/"
+                val wrong_images = Array<String>(3){"it =$it"}
+                for (i in 0..2) {
+                    wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+                }
+                //textView1.text = good_word
+                //textView2.text = wrong_images[0]
+                //textView3.text = wrong_images[1]
+                //textView4.text = wrong_images[2]
+                val wrong_image1 = wrong_images[0]
+                val wrong_image2 = wrong_images[1]
+                val wrong_image3 = wrong_images[2]
+                val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
+                val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
+                val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
+                val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
+                //imageView.setImageResource(imageResource)
+                //imageView2.setImageResource(ximageResource)
+                //imageView3.setImageResource(yimageResource)
+                //imageView4.setImageResource(zimageResource)
             }
-            textView1.text = good_word
-            textView2.text = wrong_images[0]
-            textView3.text = wrong_images[1]
-            textView4.text = wrong_images[2]
-            val wrong_image1 = wrong_images[0]
-            val wrong_image2 = wrong_images[1]
-            val wrong_image3 = wrong_images[2]
-            val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
-            val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
-            val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
-            val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
-            imageView.setImageResource(imageResource)
-            imageView2.setImageResource(ximageResource)
-            imageView3.setImageResource(yimageResource)
-            imageView4.setImageResource(zimageResource)
+            val button_dom = btn_dom
+            button_dom.setOnClickListener {
+                val kat = "dom"
+                val good_word = onClickTemp(word, kat)
+                val image = ImageReader()
+                path += kat + "/"
+                val wrong_images = Array<String>(3){"it =$it"}
+                for (i in 0..2) {
+                    wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+                }
+                //textView1.text = good_word
+                //textView2.text = wrong_images[0]
+                //textView3.text = wrong_images[1]
+                //textView4.text = wrong_images[2]
+                val wrong_image1 = wrong_images[0]
+                val wrong_image2 = wrong_images[1]
+                val wrong_image3 = wrong_images[2]
+                val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
+                val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
+                val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
+                val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
+                //imageView.setImageResource(imageResource)
+                // imageView2.setImageResource(ximageResource)
+                // imageView3.setImageResource(yimageResource)
+                // imageView4.setImageResource(zimageResource)
+            }
+            val button_jedzenie = btn_jedzenie
+            button_jedzenie.setOnClickListener {
+                val kat = "jedzenie"
+                val good_word = onClickTemp(word, kat)
+                val image = ImageReader()
+                path += kat + "/"
+                val wrong_images = Array<String>(3){"it =$it"}
+                for (i in 0..2) {
+                    wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+                }
+                //textView1.text = good_word
+                //textView2.text = wrong_images[0]
+                // textView3.text = wrong_images[1]
+                //textView4.text = wrong_images[2]
+                val wrong_image1 = wrong_images[0]
+                val wrong_image2 = wrong_images[1]
+                val wrong_image3 = wrong_images[2]
+                val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
+                val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
+                val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
+                val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
+                //imageView.setImageResource(imageResource)
+                // imageView2.setImageResource(ximageResource)
+                // imageView3.setImageResource(yimageResource)
+                // imageView4.setImageResource(zimageResource)
+            }
+            val button_szkola = btn_szkola
+            button_szkola.setOnClickListener {
+                val kat = "szkola"
+                val good_word = onClickTemp(word, kat)
+                val image = ImageReader()
+                path += kat + "/"
+                val wrong_images = Array<String>(3){"it =$it"}
+                for (i in 0..2) {
+                    wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+                }
+                //  textView1.text = good_word
+                // textView2.text = wrong_images[0]
+                // textView3.text = wrong_images[1]
+                //  textView4.text = wrong_images[2]
+                val wrong_image1 = wrong_images[0]
+                val wrong_image2 = wrong_images[1]
+                val wrong_image3 = wrong_images[2]
+                val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
+                val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
+                val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
+                val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
+                //  imageView.setImageResource(imageResource)
+                //  imageView2.setImageResource(ximageResource)
+                //   imageView3.setImageResource(yimageResource)
+                //   imageView4.setImageResource(zimageResource)
+            }
+            val button_zwierzeta = zwierze
+            button_zwierzeta.setOnClickListener {
+                val kat = "Zwierzeta"
+                val good_word = onClickTemp(word, kat)
+                val image = ImageReader()
+                path += kat + "/"
+                val wrong_images = Array<String>(3){"it =$it"}
+                for (i in 0..2) {
+                    wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
+                }
+                //  textView1.text = good_word
+                //   textView2.text = wrong_images[0]
+                //  textView3.text = wrong_images[1]
+                //   textView4.text = wrong_images[2]
+                val wrong_image1 = wrong_images[0]
+                val wrong_image2 = wrong_images[1]
+                val wrong_image3 = wrong_images[2]
+                val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
+                val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
+                val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
+                val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
+                //  imageView.setImageResource(imageResource)
+                //  imageView2.setImageResource(ximageResource)
+                //  imageView3.setImageResource(yimageResource)
+                //  imageView4.setImageResource(zimageResource)
         }
-        val button_dom = btn_dom
-        button_dom.setOnClickListener {
-            val kat = "dom"
-            val good_word = onClickTemp(word, kat)
-            val image = ImageReader()
-            path += kat + "/"
-            val wrong_images = Array<String>(3){"it =$it"}
-            for (i in 0..2) {
-                wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
-            }
-            textView1.text = good_word
-            textView2.text = wrong_images[0]
-            textView3.text = wrong_images[1]
-            textView4.text = wrong_images[2]
-            val wrong_image1 = wrong_images[0]
-            val wrong_image2 = wrong_images[1]
-            val wrong_image3 = wrong_images[2]
-            val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
-            val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
-            val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
-            val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
-            imageView.setImageResource(imageResource)
-            imageView2.setImageResource(ximageResource)
-            imageView3.setImageResource(yimageResource)
-            imageView4.setImageResource(zimageResource)
-        }
-        val button_jedzenie = btn_jedzenie
-        button_jedzenie.setOnClickListener {
-            val kat = "jedzenie"
-            val good_word = onClickTemp(word, kat)
-            val image = ImageReader()
-            path += kat + "/"
-            val wrong_images = Array<String>(3){"it =$it"}
-            for (i in 0..2) {
-                wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
-            }
-            textView1.text = good_word
-            textView2.text = wrong_images[0]
-            textView3.text = wrong_images[1]
-            textView4.text = wrong_images[2]
-            val wrong_image1 = wrong_images[0]
-            val wrong_image2 = wrong_images[1]
-            val wrong_image3 = wrong_images[2]
-            val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
-            val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
-            val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
-            val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
-            imageView.setImageResource(imageResource)
-            imageView2.setImageResource(ximageResource)
-            imageView3.setImageResource(yimageResource)
-            imageView4.setImageResource(zimageResource)
-        }
-        val button_szkola = btn_szkola
-        button_szkola.setOnClickListener {
-            val kat = "szkola"
-            val good_word = onClickTemp(word, kat)
-            val image = ImageReader()
-            path += kat + "/"
-            val wrong_images = Array<String>(3){"it =$it"}
-            for (i in 0..2) {
-                wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
-            }
-            textView1.text = good_word
-            textView2.text = wrong_images[0]
-            textView3.text = wrong_images[1]
-            textView4.text = wrong_images[2]
-            val wrong_image1 = wrong_images[0]
-            val wrong_image2 = wrong_images[1]
-            val wrong_image3 = wrong_images[2]
-            val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
-            val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
-            val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
-            val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
-            imageView.setImageResource(imageResource)
-            imageView2.setImageResource(ximageResource)
-            imageView3.setImageResource(yimageResource)
-            imageView4.setImageResource(zimageResource)
-        }
-        val button_zwierzeta = btn_Zwierzeta
-        button_zwierzeta.setOnClickListener {
-            val kat = "Zwierzeta"
-            val good_word = onClickTemp(word, kat)
-            val image = ImageReader()
-            path += kat + "/"
-            val wrong_images = Array<String>(3){"it =$it"}
-            for (i in 0..2) {
-                wrong_images[i] = image.RandImage(good_word, word.readFile(this,"slowka/"+kat+".txt"))
-            }
-            textView1.text = good_word
-            textView2.text = wrong_images[0]
-            textView3.text = wrong_images[1]
-            textView4.text = wrong_images[2]
-            val wrong_image1 = wrong_images[0]
-            val wrong_image2 = wrong_images[1]
-            val wrong_image3 = wrong_images[2]
-            val imageResource = resources.getIdentifier("@drawable/$good_word", null, this.packageName)
-            val ximageResource = resources.getIdentifier("@drawable/$wrong_image1", null, this.packageName)
-            val yimageResource = resources.getIdentifier("@drawable/$wrong_image2", null, this.packageName)
-            val zimageResource = resources.getIdentifier("@drawable/$wrong_image3", null, this.packageName)
-            imageView.setImageResource(imageResource)
-            imageView2.setImageResource(ximageResource)
-            imageView3.setImageResource(yimageResource)
-            imageView4.setImageResource(zimageResource)
+
         }
 
     }
